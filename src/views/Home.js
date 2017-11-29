20b9aff1c486252d59d2986677325d98510a0838
Rodrigo Muniz <muniz95@bsd.com.br>
2017-11-29 20:23:41 +0000

    Added pause and resume events

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,42 +9,72 @@ class Home extends Component {
         this.state = {
             text: [],
             currentWordPosition: 0,
-            pageSlip: 500
+            pageSlip: 500,
+            isReading: false,
+            intervalId: 0
         }
     }
     
     componentWillMount() {
-        const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in ligula orci. Praesent quis purus neque. Duis quis libero vel velit feugiat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus libero quam, tincidunt in dignissim sed, blandit ut mi. Nam id eros tempor, varius elit ac, sodales risus. Vestibulum a suscipit mi. Etiam ac purus dictum, iaculis nisl in, egestas odio.'
+        // const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in ligula orci. Praesent quis purus neque. Duis quis libero vel velit feugiat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus libero quam, tincidunt in dignissim sed, blandit ut mi. Nam id eros tempor, varius elit ac, sodales risus. Vestibulum a suscipit mi. Etiam ac purus dictum, iaculis nisl in, egestas odio.'
+        const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
         this.setState({
             text: text.split(' ')
         })
     }
     
     componentDidMount() {
-        this.showText()
+        // this.showText()
     }
     
     render() {
-        const { text, currentWordPosition } = this.state
+        const { text, currentWordPosition, isReading } = this.state
         const currentWord = text[currentWordPosition]
+        const button = isReading
+            ? 
+                <div>
+                    <button onClick={() => this.pauseText()}>Pausar</button>
+                </div>
+            :
+                <div>
+                    <button onClick={() => this.showText()}>Iniciar</button>
+                </div>
         return (
             <div>
-                {currentWord}
+                <h1 class="current-word">{currentWord}</h1>
                 {/*
                 Echtamos ahí na página {this.props.page}
                 <div><button onClick={this.props.dispatchPreviousPage}>Página anterior</button></div>
                 <div><button onClick={this.props.dispatchNextPage}>Página seguinte</button></div>
                 */}
+                { button }
             </div>
         )
     }
     
     showText() {
-        setInterval(() => {
-            this.setState({
-                currentWordPosition: this.state.currentWordPosition + 1
-            })
+        this.setState({
+            isReading: true
+        })
+        const intervalId = setInterval(() => {
+            const { currentWordPosition, text } = this.state
+            if (currentWordPosition < text.length - 1) {
+                this.setState({
+                    currentWordPosition: this.state.currentWordPosition + 1
+                })
+            } else {
+                this.setState({currentWordPosition: 0})
+                this.pauseText()
+            }
         }, this.state.pageSlip)
+        this.setState({intervalId})
+    }
+    
+    pauseText() {
+        this.setState({
+            isReading: false
+        })
+        clearInterval(this.state.intervalId)
     }
 }
 
