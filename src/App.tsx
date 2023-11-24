d4f871dfb9ce6de93c710b9c4e990ac01daaee09
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2023-11-24 05:49:05 -0300

    Added an input for interval in ms between words

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 
 const App = () => {
   const [pageSlip, setPageSlip] = React.useState<number>(500);
+  const [isFooterVisible, setIsFooterVisible] = React.useState<boolean>(true);
   const [text, setText] = React.useState<string[]>([]);
   const [currentWordPosition, setCurrentWordPosition] = React.useState(0);
   const [isReading, setIsReading] = React.useState(false);
-  const [intervalId, setIntervalId] = React.useState<NodeJS.Timeout | null>(null);
+  const currentTimer = React.useRef<NodeJS.Timeout>();
 
   const startReadout = (startPosition: number) => {
-    const thread = setInterval(function () {
+    currentTimer.current = setInterval(function () {
       if (startPosition < text.length - 1) {
         setCurrentWordPosition(state => state + 1);
       } else {
@@ -18,26 +19,23 @@ const App = () => {
       }
       startPosition++;
     }, pageSlip)
-    setIntervalId(thread);
+  }
+
+  const handlePageSlip = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setPageSlip(target.valueAsNumber);
   }
 
   const showText = () => {
-    console.log('iniciando leitura');
-
     setIsReading(true);
     startReadout(0);
   };
 
   const stopText = () => {
-    console.log('leitura pausada');
-
     setIsReading(false);
-    clearInterval(intervalId!)
+    clearInterval(currentTimer.current);
   };
 
   const resumeText = () => {
-    console.log('continuando leitura');
-
     setIsReading(true);
     startReadout(currentWordPosition);
   };
@@ -46,6 +44,9 @@ const App = () => {
     const sampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
     setText(sampleText.split(' '));
   }, []);
+  React.useEffect(() => {
+    setIsFooterVisible(!isReading);
+  }, [isReading]);
 
   const currentWord = text[currentWordPosition]
   const buttonAction = isReading
@@ -57,15 +58,12 @@ const App = () => {
   return (
     <div className="app">
       <div className="container">
-        <div>
-          <span onClick={() => buttonAction()} className="current-word">{currentWord}</span>
-        </div>
-        <div>
-          <div id="add-text">
-            <p className="plus">+</p>
-          </div>
-        </div>
+        <span onClick={() => buttonAction()} className="current-word">{currentWord}</span>
       </div>
+      <footer hidden={!isFooterVisible}>
+        <input type="number" name="pageSlip" id="pageSlip" defaultValue={pageSlip}
+          onChange={handlePageSlip} />
+      </footer>
     </div>
   )
 };
